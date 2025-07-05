4888593b0d3452a62da6d79814f8cb07c3052ea1
Riyas khan <m.driyaskhan55@gmail.com>
2025-07-05 07:13:01 +0530

    updated

--- main.js
+++ main.js
@@ -1,5 +1,5 @@
 var typed = new Typed(".text",{
-    strings:["Full Stack Developer", "College Student", "Programmer"],
+    strings:["Full Stack Developer", "Programmer","Web developer"],
     typeSpeed:20,
     backSpeed:20,
     backdDelay:1000,
